3ecea42a79bd5cfd38d1637d030f0dbc9ebd1eee
Danny <danny@west-industries.com>
2025-05-30 09:47:04 -0700

    Double button size, restore mic meter, transcript, and original voice flow

--- voice-crossword-ai-blind-voice/service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open('v2').then(cache => cache.addAll(['/', '/index.html', '/manifest.json'])));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
-});
